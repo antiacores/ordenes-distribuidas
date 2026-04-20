379d721b98f71aa3e7c338244ae67dd6aa45a438
antiacores <antiacores0@gmail.com>
2026-04-19 21:35:53 -0600

    Siguientes pasos

---
+++ auth-service/app/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class UserRegister(BaseModel):
+    username: str
+    email: str
+    name: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
