62c328f3a85711fb2ad7be6498134a29a770291b
antiacores <antiacores0@gmail.com>
2026-04-19 19:32:16 -0600

    Configuraciones principales

--- auth-service/app/models.py
+++ auth-service/app/models.py
@@ -9,7 +9,6 @@ class Users(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(35), nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
-    token: Mapped[str] = mapped_column(String(400), nullable=True)
